574da66dcca97f83631c49231d12db3a8b7b7b1b
Saishree Ramkumar <bhuvansaishree@gmail.com>
2024-10-15 21:10:17 -0400

    Added code to create login page.

--- main.py
+++ main.py
@@ -1,74 +1,32 @@
+from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.properties import StringProperty
-
+from kivy.core.window import Window
 from kivymd.app import MDApp
 from kivymd.uix.navigationbar import MDNavigationBar, MDNavigationItem
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import ScreenManager
+Window.size = (350, 580)
 
-class BaseMDNavigationItem(MDNavigationItem):
-    icon = StringProperty()
-    text = StringProperty()
-
-
-class BaseScreen(MDScreen):
-    ...
-
-
-KV = '''
-<BaseMDNavigationItem>
-
-    MDNavigationItemIcon:
-        icon: root.icon
-
-    MDNavigationItemLabel:
-        text: root.text
-
-
-<BaseScreen>
-
-    MDLabel:
-        text: root.name
-        halign: "center"
-
-
-MDBoxLayout:
-    orientation: "vertical"
-    md_bg_color: self.theme_cls.backgroundColor
-
-    MDScreenManager:
-        id: screen_manager
-
-        BaseScreen:
-            name: "Home"
-
-        BaseScreen:
-            name: "My Tickets"
-        
-        BaseScreen:
-            name: "About"
-
-
-    MDNavigationBar:
-        on_switch_tabs: app.on_switch_tabs(*args)
-
-        BaseMDNavigationItem
-            icon: "home"
-            text: "Home"
-            active: True
-
-        BaseMDNavigationItem
-            icon: "ticket"
-            text: "My Tickets"
-            
-        BaseMDNavigationItem
-            icon: "group"
-            text: "About"
-'''
-
-
-class myMetroTransitApp(MDApp):
+class MyMMTApp(MDApp):
+    
     def build(self):
-        return Builder.load_file('main.kv')
+        global screen_manager
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(Builder.load_file("app-splash.kv"))
+        screen_manager.add_widget(Builder.load_file("app-login.kv"))
+        screen_manager.add_widget(Builder.load_file("app-home.kv"))
+        return screen_manager
+    
+    def on_start(self):
+        Clock.schedule_once(self.login, 5)
+        
+    def login(self, *args):
+        screen_manager.current = "app-login"
+        
+    def homes(self, *args):
+        screen_manager.current = "app-home"
+            
+if __name__ == "__main__":
+    MyMMTApp().run()
 
-
-myMetroTransitApp().run()
